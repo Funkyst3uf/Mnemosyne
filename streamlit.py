9ba87651b2b762bc809810f041d106f26e1c621b
Funkyst3uf <87596636+Funkyst3uf@users.noreply.github.com>
2022-08-21 15:01:06 +0200

    Exif metadata update

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,7 @@
 # NOM : streamlit.py
 # ROLE : application FastAPI sur le thème de la musique (playlist)
 # AUTEUR : Jonathan Ntoula,  dans le cadre du cours Outils Collaboratifs (P. Kislin)
-#  Université Paris 8, IED
+# Université Paris 8, IED
 # VERSION : 0.1 du 20/08/2022
 # Exécution (w/ Uvicorn) : uvicorn ma-playlist-app --reload
 
@@ -10,6 +10,8 @@ from exif import Image
 from PIL import Image as P_Image
 from geopy.geocoders import Nominatim
 
+st.markdown("<h1 style='text-align: center; color: black;'>Outils Collaboratifs : TD 4</h1>", unsafe_allow_html=True)
+st.write('') # saute une ligne
 
 # crée des conteneurs (colonnes)
 col1, col2, col3 = st.columns(3)
@@ -18,7 +20,7 @@ col1, col2, col3 = st.columns(3)
 image = P_Image.open('mnemo.jpg')   # placez l'image dans le repertoire d'exécution de streamlit
 
 with col2 : # place l'image dans le conteneur 2 (au cente de la page)
-    st.image(image, caption='Mnemosyne, de D. G. Rossetti')
+    st.image(image, caption='Mnemosyne, D. G. Rossetti (1875-1881)')
     
 # appel de l'outil Nominatim (pour géolocation)
 loc = Nominatim(user_agent="GetLoc")
@@ -28,35 +30,37 @@ loc = Nominatim(user_agent="GetLoc")
 with open('./mnemo.jpg', 'rb') as img_file:
     img = Image(img_file)
 
-st.title("Modifiez les tags EXIF de l'image")
+st.write('\n') # saute une ligne
+st.markdown("<h2 style='text-align: center; color: black;'>Modifiez les tags EXIF de l'image</h2>", unsafe_allow_html=True)
 
 # Création / modification des tags Exif selon saisie utilisateur
-Make = st.text_input("Constructeur de l'appareil photo : ")
-img.Make = Make
+st.write('') # saute une ligne
+copyright = st.text_input("Copyright de l'image : ")
+img.copyright = copyright
 
-Model = st.text_input("Modèle de l'appareil : ")
-img.Model = Model
+artist = st.text_input("Nom du photographe : ")
+img.artist = artist
 
-DateTime = st.text_input("Date des dernières modifications (YYYY:MM:DD HH:MM:SS): " )
-img.DateTime = DateTime
+image_description = st.text_input("Description de l'image : ")
+img.image_description = image_description
 
-truc = st.text_input("truc de l'image : ")
-img.truc = truc
+datetime = st.text_input("Date des dernières modifications (YYYY:MM:DD HH:MM:SS): " )
+img.datetime = datetime
 
-Copyright = st.text_input("Copyright de l'image : ")
-img.Copyright = Copyright
+make = st.text_input("Constructeur de l'appareil photo utilisé : ")
+img.make = make
 
-Artist = st.text_input("Artiste photographié ou auteur de l'oeuvre photgraphiée : ")
-img.Artist = Artist
+model = st.text_input("Modèle de l'appareil : ")
+img.model = model
 
-Comment = st.text_input("Commentaire : ")
-img.Comment = Comment
+software = st.text_input("Software de l'appareil : ")
+img.software = software
 
 adresse = st.text_input("Quelle est votre adresse (ville ou adresse complète) ? ")
 getLoc = loc.geocode(adresse)
 if adresse :
-    img.GPSLatitude = getLoc.latitude
-    img.GPSLongitude = getLoc.longitude
+    img.gps_latitude = getLoc.latitude
+    img.gps_longitude = getLoc.longitude
 
 # Liste les tags Exif  de l'image (pour test)
 #st.write(sorted(img.list_all()))
@@ -65,21 +69,23 @@ if adresse :
 with open(f'./mnemo.jpg', 'wb') as new_img_file:
     new_img_file.write(img.get_file())
     
-    
-
 # réouverture de l'image en mode lecture pour vérifier les métadonnées
 with open('./mnemo.jpg', 'rb') as img_file:
     img = Image(img_file)
     
 # Affiche les métadonnées à jour
-st.write(f'Make: {img.get("Make")}')
-st.write(f'Model : {img.get("Model")}')
-st.write(f'DateTime : {img.get("DateTime")}')
-st.write(f'Orientation : {img.get("Orientation")}')
-st.write(f'Compression : {img.get("Compression")}')
-st.write(f'truc : {img.get("truc")}')
-st.write(f'Copyright : {img.get("Copyright")}')
-st.write(f'Artist : {img.get("Artist")}')
-st.write(f'Comment : {img.get("Comment")}')
-st.write(f'GPSLatitude : {img.get("GPSLatitude")}')
-st.write(f'GPSLongitude : {img.get("GPSLongitude")}')
+st.write('') # saute une ligne
+st.header('Rappel des métadonnées enregistrées')
+
+st.write(f'Copyright : {img.get("copyright")}')
+st.write(f'Artiste : {img.get("artist")}')
+st.write(f'Description de l\'image : {img.get("image_description")}')
+st.write(f'Date de dernière modification : {img.get("datetime")}')
+st.write(f'Marque de l\'appareil photo : {img.get("make")}')
+st.write(f'Modèle de l\'appareil : {img.get("model")}')
+st.write(f'Version du soft/firmware : {img.get("software")}')
+st.write('') # saute une ligne
+
+st.write('Coordonnées GPS du lieu de prise de vue :')
+st.write(f'Latitude : {img.get("gps_latitude")}')
+st.write(f'Longitude : {img.get("gps_longitude")}')
