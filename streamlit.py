3ade331bc1b6cdb89546df22f4eb643ef1858949
Funkyst3uf <87596636+Funkyst3uf@users.noreply.github.com>
2022-08-21 10:24:01 +0200

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,84 @@
+# NOM : streamlit.py
+# ROLE : application FastAPI sur le thème de la musique (playlist)
+# AUTEUR : Jonathan Ntoula,  dans le cadre du cours Outils Collaboratifs (P. Kislin)
+#  Université Paris 8, IED
+# VERSION : 0.1 du 20/08/2022
+# Exécution (w/ Uvicorn) : uvicorn ma-playlist-app --reload
+
+
+import streamlit as st
+from exif import Image
+from PIL import Image as P_Image
+
+#crée des conteneurs (colonnes)
+col1, col2, col3 = st.columns(3)
+
+
+# affiche l'image avec PIL
+image = P_Image.open('mnemo.jpg') 
+
+with col2 :
+    st.image(image, caption='Mnemosyne, de D. G. Rossetti')
+
+
+# traitement de l'image (exif)
+with open('./mnemo.jpg', 'rb') as img_file:
+    img = Image(img_file)
+
+
+st.title("Modifiez les tags EXIF de l'image")
+
+# Création / modification des tags Exif selon saisie utilisateur
+
+Make = st.text_input("Constructeur de l'appareil photo : ")
+img.Make = Make
+
+Model = st.text_input("Modèle de l'appareil : ")
+img.Model = Model
+
+DateTime = st.text_input("Date des dernières modifications (YYYY:MM:DD HH:MM:SS): " )
+img.DateTime = DateTime
+
+#Orientation = st.text_input("Orientation de la photo : ")
+#img.Orientation = Orientation
+
+Flash = st.text_input("Utilisation du flash : ")
+#img.Flash = Flash
+
+#Compression = st.text_input("Compression de l'image (JPEG = 6) : ")
+#img.Compression = Compression
+
+ImageDescription = st.text_input("Description de l'image : ")
+img.ImageDescription = ImageDescription
+
+Copyright = st.text_input("Copyright de l'image : ")
+img.Copyright = Copyright
+
+Artist = st.text_input("Artiste photographié ou auteur de l'oeuvre photgraphiée : ")
+img.Artist = Artist
+
+UserComment = st.text_input("Commentaire : ")
+img.UserComment = UserComment
+
+# Liste les tags Exif  de l'image (pour test)
+st.write(sorted(img.list_all()))
+
+
+# Affiche les métadonnées à jour
+st.write(f'Make: {img.get("Make")}')
+st.write(f'Model : {img.get("Model")}')
+st.write(f'DateTime : {img.get("DateTime")}')
+st.write(f'Orientation : {img.get("Orientation")}')
+st.write(f'Flash : {img.get("Flash")}')
+st.write(f'Compression : {img.get("Compression")}')
+st.write(f'ImageDescription : {img.get("ImageDescription")}')
+st.write(f'Copyright : {img.get("Copyright")}')
+st.write(f'Artist : {img.get("Artist")}')
+st.write(f'UserComment : {img.get("UserComment")}')
+   
+# Sauvegarde les modifications
+with open(f'./mnemo.jpg', 'wb') as new_img_file:
+    new_img_file.write(img.get_file())
+
+
+# https://github.com/Funkyst3uf/Mnemosyne.git
